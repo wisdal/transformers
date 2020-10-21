8cb386aaac49c0bbd78219211398c45c307402ae
Wisdom d'Almeida <wdalmeida6@gmail.com>
2020-10-21 16:47:28 -0400

    Update modeling_bert.py

--- src/transformers/modeling_bert.py
+++ src/transformers/modeling_bert.py
@@ -1185,7 +1185,7 @@ class BertForMaskedLM(BertPreTrainedModel):
             loss_fct = CrossEntropyLoss()  # -100 index = padding token
             if disambiguate:
                 masked_lm_loss = loss_fct(scores, labels.view(-1))
-                prediction_scores = scores.max(-1)
+                prediction_scores = scores.max(-1)[1]
             else:
                 masked_lm_loss = loss_fct(prediction_scores.view(-1, self.config.vocab_size), labels.view(-1))
 
